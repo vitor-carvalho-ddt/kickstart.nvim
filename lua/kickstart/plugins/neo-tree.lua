abfd30cf357ee8c5d13e1fd9e035de3c63718b83
vitor-carvalho-ddt <vitor.carvalho.ufu@gmail.com>
2025-05-05 21:00:09 -0300

    Quitting if neo-tree is the last open buffer

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,5 +1,5 @@
 -- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- https://github.com/nvim-neo-tree/neo-tree.nvim_create_autocmd
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
@@ -14,6 +14,7 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    auto_clean_after_session_restore = true,
     filesystem = {
       filtered_items = {
         visible = true,
@@ -25,6 +26,19 @@ return {
         },
       },
       use_libuv_file_watcher = true,
+    },
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          if #vim.api.nvim_list_wins() == 1 then
+            local bufname = vim.api.nvim_buf_get_name(0)
+            if string.find(bufname, 'neo%-tree') then
+              vim.cmd 'quit'
+            end
+          end
+        end,
+      },
     },
   },
 }
